e58592bb6066d6e7d84899996f1434ce858fbee9
rastrainj <rastrain@outlook.es>
2020-12-06 18:29:31 +0100

    Configuración inicial Auth0 + botones

---
+++ 07-authapp/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: [],
+})
+export class NavbarComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {}
+}
