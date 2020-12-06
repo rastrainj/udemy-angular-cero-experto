55edfc51165d0331fab3dca6dfaf024dada3e224
rastrainj <rastrain@outlook.es>
2020-12-06 18:43:40 +0100

    Añadido bloqueo de página protegida con AuthGuard.

--- 07-authapp/src/app/app-routing.module.ts
+++ 07-authapp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
@@ -7,7 +8,11 @@ import { ProtegidaComponent } from './components/protegida/protegida.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'precios', component: PreciosComponent },
-  { path: 'protegida', component: ProtegidaComponent },
+  {
+    path: 'protegida',
+    component: ProtegidaComponent,
+    canActivate: [AuthGuard],
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
