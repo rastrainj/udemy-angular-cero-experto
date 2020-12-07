39876e5338afaae5f9bec215328882126b7ab781
rastrainj <rastrain@outlook.es>
2020-12-07 10:43:28 +0100

    Validaciones en la pantalla de login.

--- 07-bonus-loginApp/src/app/pages/login/login.component.ts
+++ 07-bonus-loginApp/src/app/pages/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UsuarioModel } from "../../models/usuario.model";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  usuario: UsuarioModel = new UsuarioModel();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(this.usuario);
+    console.log(form);
   }
-
 }
