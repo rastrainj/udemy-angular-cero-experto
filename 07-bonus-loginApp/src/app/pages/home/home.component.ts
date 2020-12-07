5ca4ea454829bf787014a055ae9ce642dbcb745f
rastrainj <rastrain@outlook.es>
2020-12-07 19:31:03 +0100

    Logout.

--- 07-bonus-loginApp/src/app/pages/home/home.component.ts
+++ 07-bonus-loginApp/src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  salir() {
+    this.authService.logout();
+    this.router.navigateByUrl("/login");
   }
-
 }
