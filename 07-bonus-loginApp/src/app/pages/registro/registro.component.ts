7cf9772cc2f15161c49a556582cd2ead278c460d
rastrainj <rastrain@outlook.es>
2020-12-07 12:18:12 +0100

    Configuración de firebase + registro de nuevo usuario.

--- 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
+++ 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { UsuarioModel } from "src/app/models/usuario.model";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: "app-registro",
@@ -10,7 +11,7 @@ import { UsuarioModel } from "src/app/models/usuario.model";
 export class RegistroComponent implements OnInit {
   usuario: UsuarioModel;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -20,8 +21,13 @@ export class RegistroComponent implements OnInit {
     if (form.invalid) {
       return;
     }
-    console.log("Formulario enviado");
-    console.log(this.usuario);
-    console.log(form);
+    this.authService.nuevoUsuario(this.usuario).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        console.log(err.error.error.message);
+      }
+    );
   }
 }
--- 07-bonus-loginApp/src/app/services/auth.service.ts
+++ 07-bonus-loginApp/src/app/services/auth.service.ts
@@ -21,5 +21,15 @@ export class AuthService {
 
   login(usuario: UsuarioModel) {}
 
-  nuevoUsuario(usuario: UsuarioModel) {}
+  nuevoUsuario(usuario: UsuarioModel) {
+    const authData = {
+      ...usuario,
+      returnSecureToken: true,
+    };
+
+    return this.http.post(
+      `${this.url}/accounts:signUp?key=${this.apiKey}`,
+      authData
+    );
+  }
 }
