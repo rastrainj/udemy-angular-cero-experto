9b00aa3af13f3fde50a056987a928ea91ed51255
rastrainj <rastrain@outlook.es>
2020-12-08 12:58:20 +0100

    Reactivo. Creación del formulario - FormGroup.

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
   }
 
+  ngOnInit(): void {}
+
+  crearFormulario() {
+    this.form = this.fb.group({
+      nombre: [''],
+      apellido: [''],
+      correo: [''],
+    });
+  }
+
+  guardar() {
+    console.log(this.form);
+  }
 }
