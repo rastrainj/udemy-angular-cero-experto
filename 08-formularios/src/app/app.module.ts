ca71c8fbf0d628e6730b4c153eeb871c4370bf4b
rastrainj <rastrain@outlook.es>
2020-12-08 12:50:44 +0100

    Reactivo. Configuración inicial.

--- 08-formularios/src/app/app.module.ts
+++ 08-formularios/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,13 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 @NgModule({
   declarations: [AppComponent, TemplateComponent, ReactiveComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
