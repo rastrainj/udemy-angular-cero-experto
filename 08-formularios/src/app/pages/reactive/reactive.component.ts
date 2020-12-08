6f0e5610f67f5bd8ed118f1bed818f15a4af18cb
rastrainj <rastrain@outlook.es>
2020-12-08 13:06:25 +0100

    Reactivo. Validaciones síncronas.

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -17,9 +17,15 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario() {
     this.form = this.fb.group({
-      nombre: [''],
-      apellido: [''],
-      correo: [''],
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', Validators.required],
+      correo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
     });
   }
 
