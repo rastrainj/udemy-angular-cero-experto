c8a55ca67b5c901d35c7eaf63eb29a66096e06c9
rastrainj <rastrain@outlook.es>
2020-12-09 18:37:27 +0100

    GET. Obtener listado de héroes.

--- 09-heroesApp/src/app/services/heroes.service.ts
+++ 09-heroesApp/src/app/services/heroes.service.ts
@@ -26,4 +26,26 @@ export class HeroesService {
 
     return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
+
+  getHeroes() {
+    return this.http
+      .get(`${this.url}/heroes.json`)
+      .pipe(map(this.crearArreglo));
+  }
+
+  private crearArreglo(heroesObj: object) {
+    const heroes: HeroeModel[] = [];
+
+    if (heroesObj === null) {
+      return [];
+    }
+
+    Object.keys(heroesObj).forEach((key) => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push(heroe);
+    });
+
+    return heroes;
+  }
 }
