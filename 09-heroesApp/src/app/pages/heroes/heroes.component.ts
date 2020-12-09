7bf76406750492e87c634cf4fdacc3e0a2afb196
rastrainj <rastrain@outlook.es>
2020-12-09 20:03:07 +0100

    DELETE. Eliminar registros.

--- 09-heroesApp/src/app/pages/heroes/heroes.component.ts
+++ 09-heroesApp/src/app/pages/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 import { HeroeModel } from '../../models/heroe.model';
 
 @Component({
@@ -13,5 +14,20 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroesService.getHeroes().subscribe((resp) => (this.heroes = resp));
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number): void {
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `Está seguro que desea borrar a ${heroe.nombre}`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true,
+    }).then((resp) => {
+      if (resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id).subscribe();
+      }
+    });
   }
 }
