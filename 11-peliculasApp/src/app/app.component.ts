7fd46ecbf5862efc8468d8c544c8c0e8ed5556e5
rastrainj <rastrain@outlook.es>
2020-12-12 19:25:13 +0100

    Obtener películas en cartelera.

--- 11-peliculasApp/src/app/app.component.ts
+++ 11-peliculasApp/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'peliculasApp';
+  constructor(private peliculasService: PeliculasService) {
+    peliculasService.getCartelera().subscribe((resp) => {
+      console.log(resp);
+    });
+  }
 }
