69d67f458322a93a3de5e65c19714baf32221cab
rastrainj <rastrain@outlook.es>
2020-12-12 19:32:38 +0100

    Rutas de la aplicación.

--- 11-peliculasApp/src/app/app.module.ts
+++ 11-peliculasApp/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
