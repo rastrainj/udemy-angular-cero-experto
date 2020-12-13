e324462673b5d7fcd1672ec1045bac18639799ff
rastrainj <rastrain@outlook.es>
2020-12-13 19:18:30 +0100

    InfiniteScroll de películas.

--- 11-peliculasApp/src/app/pages/home/home.component.ts
+++ 11-peliculasApp/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera-response';
 
@@ -9,6 +9,21 @@ import { Movie } from '../../interfaces/cartelera-response';
 })
 export class HomeComponent implements OnInit {
   public movies: Movie[] = [];
+  public moviesSlideshow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const pos =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const max =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (pos > max) {
+      this.peliculasService.getCartelera().subscribe((resp) => {
+        this.movies.push(...resp.results);
+      });
+    }
+  }
 
   constructor(private peliculasService: PeliculasService) {}
 
@@ -16,6 +31,7 @@ export class HomeComponent implements OnInit {
     this.peliculasService.getCartelera().subscribe((resp) => {
       // console.log(resp.results);
       this.movies = resp.results;
+      this.moviesSlideshow = resp.results;
     });
   }
 }
--- 11-peliculasApp/src/app/services/peliculas.service.ts
+++ 11-peliculasApp/src/app/services/peliculas.service.ts
@@ -1,17 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PeliculasService {
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+
   constructor(private http: HttpClient) {}
 
+  get params() {
+    return {
+      api_key: '493a1a5f0e354264ebb4121234ada717',
+      language: 'es-ES',
+      page: this.carteleraPage.toString(),
+    };
+  }
+
   getCartelera(): Observable<CarteleraResponse> {
-    return this.http.get<CarteleraResponse>(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=493a1a5f0e354264ebb4121234ada717&language=es-ES&page=1'
-    );
+    return this.http
+      .get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing?`, {
+        params: this.params,
+      })
+      .pipe(
+        tap(() => {
+          this.carteleraPage += 1;
+        })
+      );
   }
 }
