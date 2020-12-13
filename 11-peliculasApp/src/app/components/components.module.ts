81b1c7ea0f72003c0c18d9a69bb62318d37bc2c3
rastrainj <rastrain@outlook.es>
2020-12-13 19:36:54 +0100

    Poster pipe.

--- 11-peliculasApp/src/app/components/components.module.ts
+++ 11-peliculasApp/src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
 import { RatingModule } from 'ng-starrating';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { RatingModule } from 'ng-starrating';
     SlideshowComponent,
     PeliculasPosterGridComponent,
   ],
-  imports: [CommonModule, RouterModule, RatingModule],
+  imports: [CommonModule, RouterModule, RatingModule, PipesModule],
   exports: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
 })
 export class ComponentsModule {}
