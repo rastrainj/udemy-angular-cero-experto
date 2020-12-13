23fc3b2562b46d8407e2dbfa0f13a62847a38ba5
rastrainj <rastrain@outlook.es>
2020-12-13 19:06:06 +0100

    Calificación con estrellas.

--- 11-peliculasApp/src/app/components/components.module.ts
+++ 11-peliculasApp/src/app/components/components.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
     SlideshowComponent,
     PeliculasPosterGridComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RatingModule],
   exports: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
 })
 export class ComponentsModule {}
