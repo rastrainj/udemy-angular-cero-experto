369aca6a4733cf1ddb7df49800d881d6feba2da6
rastrainj <rastrain@outlook.es>
2020-12-13 19:01:16 +0100

    PosterGrid component.

--- 11-peliculasApp/src/app/components/components.module.ts
+++ 11-peliculasApp/src/app/components/components.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SlideshowComponent],
+  declarations: [
+    NavbarComponent,
+    SlideshowComponent,
+    PeliculasPosterGridComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SlideshowComponent],
+  exports: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
 })
 export class ComponentsModule {}
