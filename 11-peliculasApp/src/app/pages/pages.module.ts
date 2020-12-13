1670519049f81b936eb88d9fdd86ae59accc7e6c
rastrainj <rastrain@outlook.es>
2020-12-13 20:19:56 +0100

    Diseño de la pantalla de detalle.

--- 11-peliculasApp/src/app/pages/pages.module.ts
+++ 11-peliculasApp/src/app/pages/pages.module.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { PeliculaComponent } from './pelicula/pelicula.component';
 import { BuscarComponent } from './buscar/buscar.component';
 import { ComponentsModule } from '../components/components.module';
+import { PipesModule } from '../pipes/pipes.module';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [HomeComponent, PeliculaComponent, BuscarComponent],
-  imports: [CommonModule, ComponentsModule],
+  imports: [CommonModule, ComponentsModule, PipesModule, RatingModule],
 })
 export class PagesModule {}
--- 11-peliculasApp/src/app/pages/pelicula/pelicula.component.ts
+++ 11-peliculasApp/src/app/pages/pelicula/pelicula.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { MovieResponse } from 'src/app/interfaces/movie-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
@@ -8,15 +10,22 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
   styleUrls: ['./pelicula.component.css'],
 })
 export class PeliculaComponent implements OnInit {
+  public pelicula: MovieResponse;
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private peliculasService: PeliculasService
+    private peliculasService: PeliculasService,
+    private location: Location
   ) {}
 
   ngOnInit(): void {
     const { id } = this.activatedRoute.snapshot.params;
     this.peliculasService.getPeliculaDetalle(id).subscribe((movie) => {
-      console.log(movie);
+      this.pelicula = movie;
     });
+  }
+
+  onRegresar() {
+    this.location.back();
   }
 }
