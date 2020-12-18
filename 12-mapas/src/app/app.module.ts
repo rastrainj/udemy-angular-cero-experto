a68fa01fb3c6d3223cce338d9927de5d7813b6b9
rastrainj <rastrain@outlook.es>
2020-12-18 18:22:59 +0100

    Añadidos componentes Material de prueba.

--- 12-mapas/src/app/app.module.ts
+++ 12-mapas/src/app/app.module.ts
@@ -5,9 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+// ANgular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
