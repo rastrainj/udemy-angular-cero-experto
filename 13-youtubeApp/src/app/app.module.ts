7ac7f8e8a87271e73012d9d7beead1860443d07d
rastrainj <rastrain@outlook.es>
2020-12-28 20:18:56 +0100

    Servicio para obtener videos de YouTube.

--- 13-youtubeApp/src/app/app.module.ts
+++ 13-youtubeApp/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- 13-youtubeApp/src/app/services/youtube.service.ts
+++ 13-youtubeApp/src/app/services/youtube.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class YoutubeService {
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyBbK25Ga3YryhimUuZrOMyPDDU_t44rbwM';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getVideos() {
+    const url = `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlist)
+      .set('key', this.apiKey);
+    return this.http.get(url, { params });
+  }
 }
